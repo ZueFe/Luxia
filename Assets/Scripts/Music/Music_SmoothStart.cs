11cc07d1a0f70f9661fdbbc2e858b58fcdc94425
ponozzska <ex3me@centrum.sk>
2014-11-14 15:53:09 +0100

    Music added

---
+++ Assets/Scripts/Music/Music_SmoothStart.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class Music_SmoothStart : MonoBehaviour {
+
+	public float SmoothingTime;
+	
+	private float t;
+	private float originalVolume;
+
+	void Start(){
+		originalVolume = GetComponent<AudioSource>().volume;
+		GetComponent<AudioSource>().volume = 0f;
+		GetComponent<AudioSource>().Play();
+	}
+
+	void Update(){
+		GetComponent<AudioSource>().volume = Mathf.SmoothDamp(GetComponent<AudioSource>().volume, originalVolume, ref t, SmoothingTime);
+	}
+
+
+}
