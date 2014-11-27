d081cf0ae5798f112f58fabfac32015f97088680
ponozzska <ex3me@centrum.sk>
2014-11-27 19:50:35 +0100

    Crystals are now recharging

---
+++ Assets/Scripts/Crystals/Crystal_Recharge.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Crystal_Recharge : MonoBehaviour {
+
+	public float RechargeTime;
+
+	private float timer;
+
+	void Update () {
+		if(!gameObject.GetComponentInChildren<Light>().enabled){
+			timer += Time.deltaTime;
+		}else{
+			timer = 0;
+		}
+
+		if(timer >= RechargeTime){
+			gameObject.GetComponentInChildren<Light>().enabled = true;
+			gameObject.GetComponentInChildren<BoxCollider>().enabled = true;
+		}
+	}
+}
