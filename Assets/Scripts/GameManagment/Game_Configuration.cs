f15ea10e487748c045fe95f464a89ceba9cc66ee
ponozzska <ex3me@centrum.sk>
2014-11-28 18:46:06 +0100

    Config file added

---
+++ Assets/Scripts/GameManagment/Game_Configuration.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game_Configuration : MonoBehaviour {
+
+	public float MusicVolume;
+	public float SFXVolume;
+	public bool MusicOn;
+
+	// Use this for initialization
+	void Start () {
+		if(PlayerPrefs.HasKey("MusicOn")){
+			MusicOn = PlayerPrefs.GetInt("MusicOn") > 0;
+		}else{
+			MusicOn = true;
+		}
+
+		if(PlayerPrefs.HasKey("MusicVolume")){
+			MusicVolume = PlayerPrefs.GetFloat("MusicVolume");
+		}else{
+			MusicVolume = 1f;
+		}
+
+		if(PlayerPrefs.HasKey("SFXVolume")){
+			SFXVolume = PlayerPrefs.GetFloat("SFXVolume");
+		}else{
+			SFXVolume = 1f;
+		}
+
+		GameObject.DontDestroyOnLoad(this.gameObject);
+
+		//change in case you wanna add animations before starting main menu
+		Application.LoadLevel("mainMenu");
+	}
+}
