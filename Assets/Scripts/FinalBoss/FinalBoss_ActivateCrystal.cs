cb5bd2525cec1a1efc5459594da4413f1f221964
ponozzska <ex3me@centrum.sk>
2014-12-16 00:25:18 +0100

    Added activation of crystal at Final boss

---
+++ Assets/Scripts/FinalBoss/FinalBoss_ActivateCrystal.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FinalBoss_ActivateCrystal : MonoBehaviour {
+
+	[HideInInspector]
+	public bool Activated;
+
+	void OnTriggerStay(Collider col){
+		if(col.tag == Global_Variables.PLAYER_TAG && Input.GetButtonDown(Global_Variables.BYPASS_OBSTICLE)){
+
+			Player_Stats ps = col.gameObject.GetComponent<Player_Stats>();
+			float energyCost = GetComponent<Obsticle_Stats>().EnergyCost;
+
+			if(ps.GetEnergy() >= energyCost){
+				ps.ChangeEnergy(-1 * energyCost);
+
+				FinalBoss_GrowCrystalLight[] crystals = GetComponentsInChildren<FinalBoss_GrowCrystalLight>();
+
+				foreach(FinalBoss_GrowCrystalLight c in crystals){
+					c.activated = true;
+				}
+			}
+		}
+	}
+}
--- Assets/Scripts/FinalBoss/FinalBoss_GrowCrystalLight.cs
+++ Assets/Scripts/FinalBoss/FinalBoss_GrowCrystalLight.cs
@@ -4,26 +4,53 @@ using System.Collections;
 public class FinalBoss_GrowCrystalLight : MonoBehaviour {
 
 	public float GrowingSpeed;
+	public float ShuttingSpeed;
+	[HideInInspector]
+	public bool activated;
 
 	private Vector3 finalScale;
-	private bool activated = true;
+	private float lightIntensity;
+
 	private Vector3 t;
+	private Vector3 u;
 	private Light l;
+	private float i;
+	private float k;
 
 	// Use this for initialization
 	void Start () {
+		activated = false;
+
 		finalScale = transform.localScale;
 		transform.localScale = Vector3.zero;
 		l = GetComponent<Light>();
+		lightIntensity = l.intensity;
+		l.intensity = 0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(activated){
-			if(!l.enabled){
-				l.enabled = true;
-			}
-			transform.localScale = Vector3.SmoothDamp(transform.localScale, finalScale, ref t, GrowingSpeed);
+			GrowCrystal();
+		}else{
+			ShutDownCrystal();
 		}
+	}
+
+	private void GrowCrystal(){
+		if(!l.enabled){
+			l.enabled = true;
+		}
+		transform.localScale = Vector3.SmoothDamp(transform.localScale, finalScale, ref t, GrowingSpeed);
+		l.intensity = Mathf.SmoothDamp(l.intensity, lightIntensity, ref i, GrowingSpeed);
+
+	}
+
+	private void ShutDownCrystal(){
+		if(l.enabled){
+			l.enabled = false;
+		}
+		transform.localScale = Vector3.SmoothDamp(transform.localScale, Vector3.zero, ref u, ShuttingSpeed);
+		l.intensity = Mathf.SmoothDamp(l.intensity, 0f, ref k, ShuttingSpeed);
 	}
 }
